a62745f3bdbe9609efb2274a6e80df0862b89580
reyes <reyes@LAPTOP-SOI4FI73>
2021-01-30 17:11:20 -0600

    version 1

---
+++ src/mx/edu/cesba/model/Employee.java
@@ -0,0 +1,21 @@
+/*
+Reyes Baltazar Baltazar
+2022705
+ */
+package mx.edu.cesba.model;
+
+public class Employee {
+
+    public int id;
+    public String firstName;
+    public String lastName;
+    public int age;
+    public int phoneNumber;
+    public double salary;
+    public String hiredate;
+
+    public void getDetails() {//metodo
+
+    }
+
+}
