e12a2973ba3493bd257f5b6187ea340326567e25
reyes <reyes@LAPTOP-SOI4FI73>
2021-01-30 17:04:09 -0600

    version 1

---
+++ src/mx/edu/cesba/model/Job.java
@@ -0,0 +1,18 @@
+/*
+Reyes Baltazar Baltazar
+2022705
+ */
+package mx.edu.cesba.model;
+
+
+public class Job {
+    
+    public int id;
+    public String title;
+    public double minSalary;
+    public double maxSalary;
+    
+    public void getDetails(){
+        
+    }
+}
