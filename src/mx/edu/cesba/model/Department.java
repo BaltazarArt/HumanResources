28ae6cae2274c2ed8daa283631ed6a52dad09bee
reyes <reyes@LAPTOP-SOI4FI73>
2021-01-30 17:13:37 -0600

    version 1

---
+++ src/mx/edu/cesba/model/Department.java
@@ -0,0 +1,17 @@
+/*
+Reyes Baltazar Baltazar
+2022705
+ */
+package mx.edu.cesba.model;
+
+
+public class Department {
+    
+    public int id;
+    public String title;
+    public String location;
+    
+    public void getDetails(){
+        
+    }
+}
